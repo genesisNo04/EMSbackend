011e211a6cd9239068a91a8076ffde984f5d2c97
namnguyen <namhoanghainguyen@gmail.com>
2025-04-29 23:29:09 -0600

    start new project for EMS backend

---
+++ src/main/java/com/example/EmployeeManagementSystem/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.example.EmployeeManagementSystem.controller;
+
+import com.example.EmployeeManagementSystem.model.Employee;
+import com.example.EmployeeManagementSystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
